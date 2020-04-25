308cae9264a1d1cefd52bee624a83e1ce5fe8db3
Hasti Narimanzadeh <hasti.nz@gmail.com>
2020-04-25 19:40:54 +0300

    cleans memory-hog script and adds sleep option

--- slurm/memory-hog.py
+++ slurm/memory-hog.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env python3
-from __future__ import print_function, division
 import argparse
-import random
-from random import uniform
-import sys
-
-parser = argparse.ArgumentParser()
-parser.add_argument('mem', help="Use this much memory")
-args = parser.parse_args()
-
-
-mem = args.mem
-if mem.endswith('b'):            mem = int(mem[:-1])
-elif mem.lower().endswith('k'):  mem = int(mem[:-1]) * 1000**1
-elif mem.lower().endswith('m'):  mem = int(mem[:-1]) * 1000**2
-elif mem.lower().endswith('g'):  mem = int(mem[:-1]) * 1000**3
-elif mem.lower().endswith('t'):  mem = int(mem[:-1]) * 1000**4
-else:                            mem = int(mem)
-
-try:
-    range = xrange
-except NameError:
-    pass
-
-print("Trying to hog %d bytes of memory"%mem)
-
-allocated = 1
-array = [ bytearray(1) ]
-import random
+from time import sleep
 import gc
+import platform
 import resource
-while True:
-    allocated_next = allocated * 2
-    array.append(bytearray(allocated_next-allocated))
-    allocated = allocated_next
-    gc.collect()
-    actual_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss * 1024
-    print("Using %d bytes so far (allocated: %s)"%(actual_bytes, allocated))
-    if actual_bytes > mem:
-        break
 
+if __name__ == "__main__":
+      
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mem', metavar="memory", 
+            help="Use this much memory")
+    parser.add_argument('--sleep', 
+            help="Sleep this many seconds", type=int)
+    args = parser.parse_args()
+
+    # calculate the amount of memory requested in bytes
+    mem = args.mem.lower()
+
+    if mem.endswith('b'):
+        mem = int(mem[:-1])
+
+    elif mem.endswith('k'):
+        mem = int(mem[:-1])*1000**1
+
+    elif mem.endswith('m'):
+        mem = int(mem[:-1])*1000**2
+
+    elif mem.endswith('g'):
+        mem = int(mem[:-1])*1000**3
+
+    elif mem.endswith('t'):
+        mem = int(mem[:-1])*1000**4
+ 
+    else:
+        mem = int(mem)
+
+    print("Trying to hog %d bytes of memeory" %mem)
+
+    allocated = 1
+    array = [bytearray(1)]
+
+    while True:
+        array.append(bytearray(allocated))
+        allocated *= 2
+        gc.collect()
+        actual_bytes = resource.getrusage(
+                resource.RUSAGE_SELF).ru_maxrss*(1024 if \
+                        platform.system() == 'Linux' else 1)
+        print("Using %d bytes so far (allocated: %s)" 
+                %(actual_bytes, allocated))
+        if actual_bytes > mem:
+            break
+
+    if args.sleep:
+        time = args.sleep
+        sleep(time)
