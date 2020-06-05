69f7bf30f85254b1d0a7d8b75bbe8ba10ee4a8cb
Simo Tuomisto <simo.tuomisto@aalto.fi>
2020-06-05 12:53:00 +0300

    gpu/helloworld: Changed job name and time requirements

--- gpu/helloworld/helloworld.sh
+++ gpu/helloworld/helloworld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=00:35:00
-#SBATCH --job-name=gpuTest
+#SBATCH --time=00:05:00
+#SBATCH --job-name=helloworld
 #SBATCH --mem-per-cpu=500M
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
